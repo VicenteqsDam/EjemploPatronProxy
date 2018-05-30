0541e2befc71a73ae21da022e9c357be3fb47da7
Vicente Quiles Sáez <vicenteqsdam@gmail.com>
2018-05-30 22:47:06 +0200

    Chain y template

--- src/main/java/es/vqs/chain/Dispensador50.java
+++ src/main/java/es/vqs/chain/Dispensador50.java
@@ -1,5 +1,18 @@
 package es.vqs.chain;
 
 public class Dispensador50 implements Dispensador {
+	
+	private Dispensador siguiente;
+
+	@Override
+	public void siguienteEslabon(Dispensador siguienteDispensador) {
+		this.siguiente = siguienteDispensador;
+	}
+
+	@Override
+	public void servirBilletes(int cantidad) {
+		//devolver o llamar al siguiente;
+		
+	}
 
 }
